4dceffa0bb732deb8925d0a67f6caacdb83d237f
Chi Zeng <4221553+chihuahua@users.noreply.github.com>
2018-03-21 23:40:56 -0500

    Randomly sample examples

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
 """Reads datasets and provides them."""
 
 import csv
+import numpy as np
 
 
 class Dataset(object):
@@ -16,7 +17,6 @@ class Dataset(object):
         on the true label for that row.
     """
     reader = csv.reader(open(data_file_path), delimiter=' ')
-
     sequences = []
     labels = []
 
@@ -30,3 +30,26 @@ class Dataset(object):
 
     self._sequences = np.asarray(sequences)
     self._labels = np.asarray(labels)
+
+  def GetCount(self):
+    """Gets the count of examples.
+
+    Returns:
+      The number of examples in the dataset.
+    """
+    return len(self._sequences)
+
+  def GetBatch(self, size):
+    """Gets a random batch of `size` examples.
+
+    Args:
+      size: The size of the batch to obtain. Must be less than the count
+        of examples.
+
+    Returns:
+      A 2-tuple of numpy arrays. The first one contains sampled
+      examples. The second one contains the labels. The rows correspond
+      to each other.
+    """
+    indices = np.random.choice(self.GetCount(), size, replace=False)
+    return (self._sequences[indices], self._labels[indices])
--- run_demo.py
+++ run_demo.py
@@ -1,7 +1,15 @@
 """This demo trains + evals a model predicting protein binding. """
 
+import os
+
 import dataset
+
+
+_DATA_DIRECTORY = 'data/wgEncodeAwgTfbsSydhHelas3Stat3IggrabUniPk'
+
 
 if __name__ == '__main__':
   train_dataset = dataset.Dataset(
-      'data/wgEncodeAwgTfbsSydhHelas3Stat3IggrabUniPk/train.data')
+      os.path.join(_DATA_DIRECTORY, 'train.data'))
+  test_dataset = dataset.Dataset(
+      os.path.join(_DATA_DIRECTORY, 'test.data'))
